e74864aa3af159fb426639464a761097be138aac
Eduardo Orejuela <eduardo.orejuela@gmail.com>
2019-12-30 00:11:23 -0500

    Rocket flying basic shapes

---
+++ js/rocketscript.js
@@ -0,0 +1,29 @@
+var timer = null;
+var countdownNumber = 10;
+
+var cambiarFase = function(state){
+ document.body.className = 'body-fase' + state;
+  				clearInterval(timer);
+  				countdownNumber = 10;
+
+ 	if (state == 2) {
+ 		timer = setInterval(function(){
+ 		document.getElementById('countdown').innerHTML = countdownNumber;
+ 		countdownNumber = countdownNumber-1;
+ 			if (countdownNumber <= 0) {
+ 				cambiarFase(3)
+ 			}
+ 		},100);
+
+ 	} else if (state == 3) {
+ 		var success = setTimeout(function(){
+ 			var randomNumber = Math.round(Math.random()*10);
+ 			if (randomNumber > 3){
+ 				cambiarFase(4)
+ 			} else {
+ 				cambiarFase(5)
+ 			}
+ 		}, 2000);
+ 	};
+
+};
